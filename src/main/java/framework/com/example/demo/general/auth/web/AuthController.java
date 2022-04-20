af5e04c284f4c1cd49c88ceaabde268f8c21526b
DESKTOP-0UMG6JC\dlthf <pizza030667@gmail.com>
2022-04-20 16:11:27 +0900

    demo setup

--- src/main/java/framework/com/example/demo/general/auth/web/AuthController.java
+++ src/main/java/framework/com/example/demo/general/auth/web/AuthController.java
@@ -1,12 +1,7 @@
-package framework.general.auth.web;
+package framework.com.example.demo.general.auth.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AuthController {
